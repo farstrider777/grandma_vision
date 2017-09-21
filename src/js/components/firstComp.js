658ba6c87b8863ac63c1cfdc729ddc0f59833621
Joshua Bradford <jjyet@yahoo.com>
2017-09-21 07:07:22 -0400

    freeze point before changing to new ui router

---
+++ src/js/components/firstComp.js
@@ -0,0 +1,9 @@
+import FirstCompController from "./firstComp.controller"
+import FirstCompTemplate from "./firstComp.html"
+import "./firstComp.scss"
+
+const FirstComp = {
+  controller: FirstCompController,
+  controllerAs: "firstCompController",
+  template: FirstCompTemplate
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,36 @@
 import angular from 'angular';
 import LikeCounter from './controllers/counter';
-console.log("hello_test");
+import FirstCompComponent from './components/firstComp';
+import FirstCompController from './components/firstComp.controller';
+//console.log("hello_test");
 
 angular
   .module('app', [])
   .controller('LikeCounter', LikeCounter)
+  .component('firstCompComponent', FirstCompComponent)
+  .controller('firstCompController', FirstCompController)
+  // .directive('item', function() {
+  //   return {
+  //     scope: {
+  //       item: '=set',
+  //       onClick: '&'
+  //     },
+  //     replace: true,
+  //     controller: function() {},
+  //     controllerAs: 'ctrl',
+  //     bindToController: true,
+  //     restrict: 'EA',
+  //     template: '<input type="checkbox" ng-click="ctrl.onClick({item: ctrl.item})" ng-checked="ctrl.item.active" /> '
+  //   };
+  // })
+  // .controller('Controller', ['$scope', function($scope) {
+  //   $scope.customer = {
+  //     name: 'Naomi',
+  //     address: '1600 Amphitheatre'
+  //   };
+  // }])
+  // .directive('myCustomer', function() {
+  //   return {
+  //     template: 'Name: {{customer.name}} Address: {{customer.address}}'
+  //   };
+  // });
